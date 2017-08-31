bcb07759cb1bbe49a81a6fb377f1e6ae2cfec2e1
Anthony <ajlusardi@gmail.com>
2017-08-31 15:09:19 -0400

    Small formatting changes

--- app/local_settings_example.py
+++ app/local_settings_example.py
@@ -36,15 +36,19 @@ ADMINS = [
 # ETH_WALLET_ADDRESS = "0x0"
 # BTC_WALLET_ADDRESS = "1"
 
-ETC_WALLET_ADDRESS = "Put the wallet to monitor here"
 # ETC/ETH Wallet addresses must be lowercase
-ETC_WALLET_ADDRESS = ETC_WALLET_ADDRESS.lower()
+ETC_WALLET_ADDRESS = "Put the wallet to monitor here".lower()
 
-ETC_MIN_BLOCK = 4376000 # Block to start monitoring at, no point in tracking old blocks
-ETC_RPC_ADDRESS = "http://localhost:8545" # Parity RPC address for ETC
+# Block to start monitoring at, no point in tracking old blocks
+ETC_MIN_BLOCK = 4376000
+# Parity RPC address for ETC
+ETC_RPC_ADDRESS = "http://localhost:8545" 
 
-BLOCK_CHECK_DELAY = 5 # Delay in seconds between checking for changes on the blockchains
+# Delay in seconds between checking for changes on the blockchains
+BLOCK_CHECK_DELAY = 5 
 
-EMAIL_TIME_LIMIT = datetime.timedelta(minutes=15) # Delay between email alerts
+# Delay between email alerts
+EMAIL_TIME_LIMIT = datetime.timedelta(minutes=15) 
 
-CELERY_BROKER_URL="Celery broker URL" # Use RabbitMQ
+# Use RabbitMQ
+CELERY_BROKER_URL="Celery broker URL" 
